a4cdcd6af0479b32b9cd1c9d3c06d7da69938e99
yutorse <yutohorse.coc@gmail.com>
2022-08-12 03:49:03 +0900

    MVP

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, LocationMessage, QuickReplyButton, QuickReply, MessageAction,
 )
 import os
+from datetime import datetime, timedelta
+import re
+import weather_api
+
 
 app = Flask(__name__)
 
@@ -19,6 +23,8 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+global user_location
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -40,12 +46,70 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    if event.message.text == '!頭痛ーる' or event.message.text == '！頭痛ーる':
+        line_bot_api.reply_message(
+            event.reply_token,
+            [
+            TextSendMessage(text="位置情報を教えてください。"),
+            TextSendMessage(text="https://line.me/R/nv/location/")
+            ]
+        )
+    elif re.compile(r"今日\(\d{1,2}\/\d{1,2}\)").search(event.message.text):
+        now = datetime.now()
+        today_weather_data = weather_api.get_pressure_status(user_location, "today")
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=f"今日 {now.month}月{now.day}日 の気圧情報は\n{today_weather_data}")
+        )
+    elif re.compile(r"明日\(\d{1,2}\/\d{1,2}\)").search(event.message.text):
+        now = datetime.now()
+        tomorrow = now + timedelta(1)
+        tomorrow_weather_data = weather_api.get_pressure_status(user_location, "tommorow")
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=f"明日 {tomorrow.month}月{tomorrow.day}日 の気圧情報は\n{tomorrow_weather_data}")
+        )
+    elif re.compile(r"明後日\(\d{1,2}\/\d{1,2}\)").search(event.message.text):
+        now = datetime.now()
+        day_after_tomorrow = now + timedelta(2)
+        day_after_tomorrow_weather_data = weather_api.get_pressure_status(user_location, "dayaftertomorrow")
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=f"明日 {day_after_tomorrow.month}月{day_after_tomorrow.day}日 の気圧情報は\n{day_after_tomorrow_weather_data}")
+        )
+
+
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_location_message(event):
+    global user_location
+    user_location = weather_api.get_city_code_2(event.message.address)
+
+    now = datetime.now()
+    tomorrow = now + timedelta(1)
+    day_after_tomorrow = now + timedelta(2)
+
+    print(event.message.address)
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(
+            text = "日時を指定してください。",
+            quick_reply=QuickReply(
+                items=[
+                    QuickReplyButton(
+                        action=MessageAction(label=f"今日({now.month}/{now.day})", text=f"今日({now.month}/{now.day})")
+                    ),
+                    QuickReplyButton(
+                        action=MessageAction(label=f"明日({tomorrow.month}/{tomorrow.day})", text=f"明日({tomorrow.month}/{tomorrow.day})")
+                    ),
+                    QuickReplyButton(
+                        action=MessageAction(label=f"明後日({day_after_tomorrow.month}/{day_after_tomorrow.day})", text=f"明後日({day_after_tomorrow.month}/{day_after_tomorrow.day})")
+                    ),
+                ]
+            )
+        )
+    )
 
 
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
+    #app.run(port=5002)
--- weather_api.py
+++ weather_api.py
@@ -1,11 +1,12 @@
-import requests, json
+import requests, json, re
+from difflib import get_close_matches
 
 # ○○市××区 → id
 def get_point_id(address):
     url = "https://zutool.jp/api/getweatherpoint/"+address
 
     try:
-        response = requests.get("https://zutool.jp/api/getweatherpoint/"+address)
+        response = requests.get(url)
         response.raise_for_status()
 
     except requests.exceptions.RequestsException as e:
@@ -19,3 +20,72 @@ def get_point_id(address):
         else:
             city_code = result_data[0]['city_code']
             return city_code
+
+
+def get_prefecture_from_location(location):
+    # location から 郵便番号を抽出
+    post_number = re.search(r"\d{3}-\d{4}", location).group()
+
+    url = "http://geoapi.heartrails.com/api/json?method=searchByPostal&postal=" + post_number
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+    except requests.exceptions.RequestException as e:
+        print("HTTP Error: ", e)
+
+    else:
+        response_data = response.json()
+        prefecture = response_data["response"]["location"][0]["prefecture"]
+        return prefecture
+
+
+def get_city_code_2(user_location):
+    prefecture = get_prefecture_from_location(user_location)
+
+    url = "https://zutool.jp/api/getweatherpoint/"+prefecture
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+    except requests.exceptions.RequestsException as e:
+        print("HTTP Error: ", e)
+
+    else:
+        response_data = json.loads(response.json()["result"])
+
+        name_list = []
+        for location_info in response_data:
+            name_list.append(location_info["name"])
+
+        user_location_name = get_close_matches(user_location, name_list, n=1, cutoff=0.1)[0]
+
+        for location_info in response_data:
+            if location_info["name"] == user_location_name: return location_info["city_code"]
+
+
+def get_weather_status(city_code):
+    url = "https://zutool.jp/api/getweatherstatus/" + city_code
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+    except requests.exceptions.RequestsException as e:
+        print("HTTP Error: ", e)
+
+    else:
+        response_data = response.json()
+        return response_data
+
+def get_pressure_status(city_code, date):
+    pressure_info = ""
+    date_weather_status = get_weather_status(city_code)[date]
+    for i in range(24):
+        pressure_info = pressure_info + f"{i}時 : {date_weather_status[i]['pressure']} hPa, 危険度{date_weather_status[i]['pressure_level']}\n"
+    return pressure_info
+
+if __name__ == '__main__':
+    
